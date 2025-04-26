a436894513ee85a3c3a55f532490cf01f4010e89
Fırat Alçın <firatalcin04@gmail.com>
2025-04-26 23:59:45 +0300

    HTTP Response Headers

--- 02_HTTP/MyFirstApp/Program.cs
+++ 02_HTTP/MyFirstApp/Program.cs
@@ -5,8 +5,12 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
+    context.Response.Headers["MyKey"] = "My Value";
+    context.Response.Headers["Server"] = "My Server";
+    context.Response.Headers["Content-Type"] = "text/html";
     context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("Hello World!");
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync("<h2>World</h2>");
 });
 
 app.Run();
