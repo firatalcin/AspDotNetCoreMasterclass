a83bccebfc1bf4b53ce0e19761287d471516cef7
Fırat Alçın <firatalcin04@gmail.com>
2025-05-03 22:29:46 +0300

    HTTP Request

--- 02_HTTP/MyFirstApp/Program.cs
+++ 02_HTTP/MyFirstApp/Program.cs
@@ -5,12 +5,22 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
+
+    // Http Response
+    /*
     context.Response.Headers["MyKey"] = "My Value";
     context.Response.Headers["Server"] = "My Server";
     context.Response.Headers["Content-Type"] = "text/html";
     context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("<h1>Hello</h1>");
-    await context.Response.WriteAsync("<h2>World</h2>");
+    */
+
+    // Http Request
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync($"<h1>{path}</h1>");
+    await context.Response.WriteAsync($"<h1>{method}</h1>");
+    
 });
 
 app.Run();
