9c1d80d32a36273ffd774b8328b04606aff0f63d
Fırat Alçın <firatalcin04@gmail.com>
2025-05-03 22:54:12 +0300

    HTTP Request Headers

--- 02_HTTP/MyFirstApp/Program.cs
+++ 02_HTTP/MyFirstApp/Program.cs
@@ -15,12 +15,39 @@ app.Run(async (HttpContext context) =>
     */
 
     // Http Request
+    /*
     string path = context.Request.Path;
     string method = context.Request.Method;
     context.Response.Headers["Content-Type"] = "text/html";
     await context.Response.WriteAsync($"<h1>{path}</h1>");
     await context.Response.WriteAsync($"<h1>{method}</h1>");
-    
+    */
+
+    // Query String
+    /*
+    context.Response.Headers["Content-Type"] = "text/html";
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<h1>Id: {id}</h1>");
+        }
+        else
+        {
+            await context.Response.WriteAsync("<h1>Id not found</h1>");
+        }
+    }
+    */
+
+    // HTTP Request Headers
+
+    context.Response.Headers["Content-Type"] = "text/html";
+    if (context.Request.Headers.ContainsKey("User-Agent"))
+    {
+        string userAgent = context.Request.Headers["User-Agent"];
+        await context.Response.WriteAsync($"<h1>Id: {userAgent}</h1>");
+    }   
 });
 
 app.Run();
