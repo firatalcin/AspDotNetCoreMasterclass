f42a678f12ec605eaa9f60a83a780f852d61af5b
Fırat Alçın <firatalcin04@gmail.com>
2026-03-05 23:46:58 +0300

    Postman

--- 02_HTTP/HTTP/Program.cs
+++ 02_HTTP/HTTP/Program.cs
@@ -55,6 +55,16 @@ app.Run(async (HttpContext context) =>
     //     await context.Response.WriteAsync($"<p>{userAgent}</p>");
     // }
     
+    //Postman
+
+    context.Response.Headers["Content-type"] = "text/html";
+    if (context.Request.Headers.ContainsKey("AuthorizationKey"))
+    {
+        string auth = context.Request.Headers["AuthorizationKey"];
+        await context.Response.WriteAsync($"<p>{auth}</p>");
+    }
+
+    
     
     
 });
