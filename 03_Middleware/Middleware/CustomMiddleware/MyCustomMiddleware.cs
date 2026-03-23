0a7d7be68b5238cd2dc53a7494a88edf36f3fc38
Fırat Alçın <firatalcin04@gmail.com>
2026-03-23 22:44:07 +0300

    Custom Middleware Class

---
+++ 03_Middleware/Middleware/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,11 @@
+﻿namespace Middleware.CustomMiddleware;
+
+public class MyCustomMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    { 
+        await context.Response.WriteAsync("My Custom Middleware - Starts");
+        await next(context);
+        await context.Response.WriteAsync("My Custom Middleware - Ends");
+    }
+}
