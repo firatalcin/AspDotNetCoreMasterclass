9938a8e09bc8e675539c6cb5da08a7ccf8fde802
Fırat Alçın <firatalcin04@gmail.com>
2026-04-06 22:53:24 +0300

    Route Parameters

--- 04_Routing/Routing/Program.cs
+++ 04_Routing/Routing/Program.cs
@@ -15,4 +15,25 @@ app.MapFallback(async context => {
     await context.Response.WriteAsync($"Request received at {context.Request.Path}");
 });
 
+//2- Route Parameters
+
+app.Map("files/{filename}.{extension}", async context =>
+{
+    string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
+    string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+
+    await context.Response.WriteAsync($"In files - {fileName} - {extension}");
+});
+
+app.Map("employee/profile/{EmployeeName}", async context => 
+{
+    string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+    await context.Response.WriteAsync($"In Employee profile - {employeeName}");
+});
+
+app.MapFallback(async (context) =>
+{
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+});
+
 app.Run();
