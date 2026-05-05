3560b069e9b8d037def85b2fafddf7a88e18f964
Fırat Alçın <firatalcin04@gmail.com>
2026-05-05 23:36:03 +0300

    Creating Controllers, Multiple Action Methods

---
+++ 05_Controllers/05_Controllers/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _05_Controllers.Controllers;
+
+[Controller]
+public class HomeController
+{
+    [Route("home")]
+    [Route("/")]
+    public string Index()
+    {
+        return "Hello from Index";
+    }
+
+    [Route("about")]
+    public string About()
+    {
+        return "Hello from About";
+    }
+
+    [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")]
+    public string Contact()
+    {
+        return "Hello from Contact";
+    }
+}
